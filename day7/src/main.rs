5272764cb88f8eac3fda396a4e411a036267e541
Matthew Vernacchia <mvernacc@mit.edu>
2020-12-13 21:54:27 -0500

    Add breadth-first search to find upstream bags.

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,9 +1,8 @@
 #[macro_use]
 extern crate lazy_static;
 
-use std::fs;
-use petgraph::graphmap::DiGraphMap;
-use petgraph::dot::{Dot, Config};
+use std::{fs, collections::HashSet};
+use petgraph::{graphmap::DiGraphMap, Direction, dot::Dot};
 use regex::Regex;
 
 
@@ -11,9 +10,37 @@ fn main() {
     let text = fs::read_to_string("./example_input.txt").unwrap();
     let graph = parse_graph_from_text(&text);
     println!("{:?}", Dot::new(&graph));
+    let containing_bags = compute_bags_that_can_contain(&graph, "shiny gold");
+    println!("{:?}", containing_bags);
 }
 
-fn parse_graph_from_text(text: &str) -> DiGraphMap::<&str, u32> {
+
+/// Compute all the bag colors which can eventually contain a bag of color `start_bag`.
+fn compute_bags_that_can_contain<'a>(graph: &DiGraphMap<&'a str, u32>, start_bag: &'a str) -> HashSet<&'a str> {
+    assert!(graph.contains_node(start_bag));
+
+    // Use breadth-first search.
+    // Queue `Q` in CLRS page 595.
+    let mut search_queue = Vec::<&str>::new();
+    // Nodes which are colored black in CLRS page 595.
+    let mut upstream_bags = HashSet::<&str>::new();
+    search_queue.push(start_bag);
+    while !search_queue.is_empty() {
+        let u = search_queue.pop().unwrap();
+        for v in graph.neighbors_directed(u, Direction::Incoming) {
+            if !upstream_bags.contains(v) {
+                search_queue.push(v);
+            }
+        }
+        upstream_bags.insert(u);
+    }
+
+    upstream_bags.remove(start_bag);
+    upstream_bags
+}
+
+
+fn parse_graph_from_text(text: &str) -> DiGraphMap<&str, u32> {
     let lines = text
         .trim()
         .split("\n")
@@ -75,5 +102,17 @@ mod tests {
         assert_eq!(2, contains.len());
         assert_eq!("bright white", contains[0].0);
         assert_eq!(1, contains[0].1);
+    }
+
+    #[test]
+    fn test_can_contain_example () {
+        let text = fs::read_to_string("./example_input.txt").unwrap();
+        let graph = parse_graph_from_text(&text);
+        let containing_bags = compute_bags_that_can_contain(&graph, "shiny gold");
+        assert_eq!(4, containing_bags.len());
+        assert!(containing_bags.contains("bright white"));
+        assert!(containing_bags.contains("muted yellow"));
+        assert!(containing_bags.contains("dark orange"));
+        assert!(containing_bags.contains("light red"));
     }
 }
