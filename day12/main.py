56bb827bb6340f34a61ef905516a11bafb2077be
Matthew Vernacchia <mvernacc@mit.edu>
2020-12-29 14:02:39 -0500

    Add day 12 part 2 solution.

--- day12/main.py
+++ day12/main.py
@@ -1,7 +1,7 @@
 import day12
 
 print('Example: ')
-dist = day12.nav_instructions_manhattan_distance("F10\nN3\nF7\nR90\nF11", verbose=True)
+dist = day12.nav_instructions_manhattan_distance_1("F10\nN3\nF7\nR90\nF11", verbose=True)
 assert dist == 25
 print('Example OK\n')
 
@@ -10,5 +10,9 @@ with open('input.txt', 'r') as input_file:
     nav_instructions_text = input_file.read()
 
 # Part 1.
-dist = day12.nav_instructions_manhattan_distance(nav_instructions_text)
+dist = day12.nav_instructions_manhattan_distance_1(nav_instructions_text)
 print(f'Part 1: Manhattan distance  = {dist:d}')
+
+# Part 2.
+dist = day12.nav_instructions_manhattan_distance_2(nav_instructions_text)
+print(f'Part 2: Manhattan distance  = {dist:d}')
--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -7,8 +7,10 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
 
+/// Solves according to rules for part 1.
 #[pyfunction(verbose=false)]
-fn nav_instructions_manhattan_distance(nav_instructions_text: &str, verbose: bool) -> PyResult<i32> {
+#[text_signature = "(nav_instructions_text, verbose, /)"]
+fn nav_instructions_manhattan_distance_1(nav_instructions_text: &str, verbose: bool) -> PyResult<i32> {
     let nav_instructions = nav_instructions_text
         .trim()
         .split("\n")
@@ -48,6 +50,59 @@ fn nav_instructions_manhattan_distance(nav_instructions_text: &str, verbose: boo
     Ok(x.abs() + y.abs())
 }
 
+/// Solves according to rules for part 2.
+#[pyfunction(verbose=false)]
+#[text_signature = "(nav_instructions_text, verbose, /)"]
+fn nav_instructions_manhattan_distance_2(nav_instructions_text: &str, verbose: bool) -> PyResult<i32> {
+    let nav_instructions = nav_instructions_text
+        .trim()
+        .split("\n")
+        .collect::<Vec<&str>>();
+
+    let mut ship: (i32, i32) = (0, 0);
+    // Waypoint (x, y) position is relative to the ship.
+    let mut waypoint: (i32, i32) = (10, 1);
+    for instruction in nav_instructions.iter() {
+        if verbose {
+            println!("ship: {}, {};    waypoint {}, {};    inst: {}",
+            ship.0, ship.1, waypoint.0, waypoint.1, instruction);
+        }
+
+        let action: char = instruction.chars().nth(0).unwrap();
+        let arg = instruction[1..].parse::<i32>().unwrap();
+        match action {
+            'N' => waypoint.1 += arg,
+            'S' => waypoint.1 -= arg,
+            'E' => waypoint.0 += arg,
+            'W' => waypoint.0 -= arg,
+            'L' => {
+                waypoint = int_rotate_point(waypoint, arg);
+            },
+            'R' => {
+                waypoint = int_rotate_point(waypoint, -1 * arg);
+            }
+            'F' => {
+                ship.0 += arg * waypoint.0;
+                ship.1 += arg * waypoint.1;
+            }
+            _ => panic!()
+        }
+    }
+
+    Ok(ship.0.abs() + ship.1.abs())
+}
+
+/// Rotate a point (x, y) about (0, 0) by `angle` degrees counterclockwise.
+fn int_rotate_point(point: (i32, i32), angle: i32) -> (i32, i32) {
+    let wrapped_angle = wrap_heading(angle);
+    let cos = int_cosine(wrapped_angle);
+    let sin = int_sine(wrapped_angle);
+    (
+        point.0 * cos - point.1 * sin,
+        point.0 * sin + point.1 * cos
+    )
+}
+
 fn wrap_heading(heading: i32) -> i32 {
      let mut new_heading = heading % 360;
     if new_heading < 0 { new_heading += 360; }
@@ -82,7 +137,8 @@ fn int_sine(angle: i32) -> i32 {
 #[pymodule]
 fn day12(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(nav_instructions_manhattan_distance, m)?)?;
+    m.add_function(wrap_pyfunction!(nav_instructions_manhattan_distance_1, m)?)?;
+    m.add_function(wrap_pyfunction!(nav_instructions_manhattan_distance_2, m)?)?;
 
     Ok(())
 }
@@ -93,10 +149,18 @@ mod tests {
     use super::*;
 
     #[test]
-    fn nav_instructions_manhattan_distance_example() {
+    fn nav_instructions_manhattan_distance_1_example() {
         let nav_instructions_text = "F10\nN3\nF7\nR90\nF11";
-        let result = nav_instructions_manhattan_distance(&nav_instructions_text, true);
+        let result = nav_instructions_manhattan_distance_1(&nav_instructions_text, true);
         let dist = result.ok().unwrap();
         assert_eq!(dist, 25);
+    }
+
+    #[test]
+    fn nav_instructions_manhattan_distance_2_example() {
+        let nav_instructions_text = "F10\nN3\nF7\nR90\nF11";
+        let result = nav_instructions_manhattan_distance_2(&nav_instructions_text, true);
+        let dist = result.ok().unwrap();
+        assert_eq!(dist, 286);
     }
 }
