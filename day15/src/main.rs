9c620ce5ab8d8fc3f67c0efff8ec0bc963bb193f
Matthew Vernacchia <mvernacc@mit.edu>
2020-12-30 17:59:16 -0500

    Add day 15 part 2 solution.

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,32 +1,35 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
-    let last_number = elf_game(&vec![20, 0, 1, 11, 6, 3]);
+    let last_number = elf_game(&vec![20, 0, 1, 11, 6, 3], 2020);
     println!("Part 1: {}", last_number);
+
+    let last_number_2 = elf_game(&vec![20, 0, 1, 11, 6, 3], 30000000);
+    println!("Part 2: {}", last_number_2);
 }
 
-const END_TURN: usize = 2020;
 
-fn elf_game(starting_numbers: &Vec<usize>) -> usize {
-    // If `turn_last_spoken[i] == k != 0`, then `i` was last spoken on turn `k`.
-    // If `turn_last_spoken[i]` then `i` has not been spoken before.
-    let mut turn_last_spoken = [0 as usize; END_TURN as usize];
+fn elf_game(starting_numbers: &Vec<usize>, end_turn: usize) -> usize {
+    // If `turn_last_spoken[i] == k`, then `i` was last spoken on turn `k`.
+    // If `turn_last_spoken` does not contain `i`, then `i` has not been spoken before.
+    let mut turn_last_spoken = HashMap::<usize, usize>::new();
     let mut turn_number = 1;
     let mut number_spoken_last_turn = 0;
     let mut number_spoken_this_turn = 0;
 
     for &number in starting_numbers.iter() {
-        turn_last_spoken[number] = turn_number;
+        turn_last_spoken.insert(number, turn_number);
         number_spoken_last_turn = number;
         turn_number += 1;
     }
 
-    while turn_number <= END_TURN {
-        if turn_last_spoken[number_spoken_last_turn] == 0 {
+    while turn_number <= end_turn {
+        if !turn_last_spoken.contains_key(&number_spoken_last_turn) {
             number_spoken_this_turn = 0;
         } else {
-            number_spoken_this_turn = turn_number - 1 - turn_last_spoken[number_spoken_last_turn];
+            number_spoken_this_turn = turn_number - 1 - turn_last_spoken.get(&number_spoken_last_turn).unwrap();
         }
-        turn_last_spoken[number_spoken_last_turn] = turn_number - 1;
+        turn_last_spoken.insert(number_spoken_last_turn, turn_number - 1);
         // println!("Turn {}: {}", turn_number, number_spoken_this_turn);
         turn_number += 1;
         number_spoken_last_turn = number_spoken_this_turn;
@@ -40,7 +43,7 @@ mod test {
 
     #[test]
     fn test_elf_game_1() {
-        let last_number = elf_game(&vec![0, 3, 6]);
+        let last_number = elf_game(&vec![0, 3, 6], 2020);
         assert_eq!(last_number, 436);
     }
 }
